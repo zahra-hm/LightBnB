5c1d9317607595e0242d007a396ac415d794b2ff
zahra-hm <zahra520@hotmail.com>
2021-01-08 09:26:18 +0000

    update lighthousebnb reservations

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -1,11 +1,17 @@
+const { Pool } = require("pg");
+// Connecting to the lightbnb database.
+const pool = new Pool({
+  user: "vagrant",
+  password: "123",
+  host: "localhost",
+  database: "lightbnb"
+});
 const properties = require('./json/properties.json');
 const users = require('./json/users.json');
-
 /// Users
-
 /**
  * Get a single user from the database given their email.
- * @param {String} email The email of the user.
+ * @param {String}email The email of the user.
  * @return {Promise<{}>} A promise to the user.
  */
 const getUserWithEmail = function(email) {
@@ -13,7 +19,6 @@ const getUserWithEmail = function(email) {
   WHERE email = $1`, [email]).then(res => res.rows[0])
 }
 exports.getUserWithEmail = getUserWithEmail;
-
 /**
  * Get a single user from the database given their id.
  * @param {string} id The id of the user.
@@ -24,33 +29,37 @@ const getUserWithId = function(id) {
     WHERE id = $1`, [id]).then(res => res.rows[0])
 }
 exports.getUserWithId = getUserWithId;
-
-
 /**
  * Add a new user to the database.
  * @param {{name: string, password: string, email: string}} user
  * @return {Promise<{}>} A promise to the user.
  */
 const addUser =  function(user) {
-  return pool.query(`INSERT INTO users (name, email, password)
-  VALUES ($1, $2, $3) RETURNING *;`, [user.name, user.email, user.password]).then(res => res.rows[0])
+return pool.query(`INSERT INTO users (name, email, password)
+VALUES ($1, $2, $3) RETURNING *;`, [user.name, user.email, user.password]).then(res => res.rows[0])
 }
 exports.addUser = addUser;
-
 /// Reservations
-
 /**
  * Get all reservations for a single user.
  * @param {string} guest_id The id of the user.
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function(guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+
+  return pool.query(`SELECT properties.*, reservations.*, avg(rating) as average_rating
+  FROM reservations
+  JOIN properties ON reservations.property_id = properties.id
+  JOIN property_reviews ON properties.id = property_reviews.property_id 
+  WHERE reservations.guest_id = $1
+  AND reservations.end_date < now()::date
+  GROUP BY properties.id, reservations.id
+  ORDER BY reservations.start_date
+  LIMIT $2`, [guest_id, limit]).then(res => res.rows)
 }
 exports.getAllReservations = getAllReservations;
 
 /// Properties
-
 /**
  * Get all properties.
  * @param {{}} options An object containing query options.
@@ -58,12 +67,6 @@ exports.getAllReservations = getAllReservations;
  * @return {Promise<[{}]>}  A promise to the properties.
  */
 const getAllProperties = function(options, limit = 10) {
-  // const limitedProperties = {};
-  // for (let i = 1; i <= limit; i++) {
-  //   limitedProperties[i] = properties[i];
-  // }
-  // return Promise.resolve(limitedProperties);
-
   return pool.query(`
   SELECT * FROM properties
   LIMIT $1
@@ -71,8 +74,6 @@ const getAllProperties = function(options, limit = 10) {
   .then(res => res.rows);
 }
 exports.getAllProperties = getAllProperties;
-
-
 /**
  * Add a property to the database
  * @param {{}} property An object containing all of the property details.
